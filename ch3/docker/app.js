c069bd237a0a9b390c584e35d56fb61d956c16ee
pea-sys <pea98258@gmail.com>
2025-12-08 17:38:03 +0900

    ch3

--- ch3/docker/app.js
+++ ch3/docker/app.js
@@ -2,10 +2,10 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n'); 1
+  res.end('Fundamentals of DevOps!\n')
 });
 
-const port = process.env.PORT || 8080; 2
+const port = process.env.PORT || 8080;
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
