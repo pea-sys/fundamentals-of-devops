722cf6744900e91facca6a8af181b939cf4a05af
pea-sys <pea98258@gmail.com>
2025-12-11 21:55:59 +0900

    ch6

--- ch6/tofu/live/child-accounts/main.tf
+++ ch6/tofu/live/child-accounts/main.tf
@@ -2,17 +2,22 @@ provider "aws" {
   region = "us-east-2"
 }
 
-module "child_accounts" {
-  source  = "brikis98/devops/book//modules/aws-organizations"
+module "function" {
+  source  = "brikis98/devops/book//modules/lambda"
   version = "1.0.0"
 
-  create_organization = true                 
+  name    = var.name
+  src_dir = "${path.module}/src"
+  runtime = "nodejs20.x"
+  handler = "index.handler"
 
-  # TODO: fill in your own account emails!
-  accounts = {                               
-    development = "username+dev@email.com"
-    staging     = "username+stage@email.com"
-    production  = "username+prod@email.com"
+  memory_size = var.memory_size  
+  timeout     = 5
+
+  environment_variables = {
+    NODE_ENV = "production"
+    ENV_NAME = var.environment   
   }
 
+  create_url = true
 }
